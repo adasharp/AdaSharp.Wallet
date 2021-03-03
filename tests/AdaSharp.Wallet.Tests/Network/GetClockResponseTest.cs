716564df97017b10c160a7a4b2932dc6a8a35902
Adrian.Balij <adrian.balij@Musket.ca>
2021-03-02 23:31:55 -0500

    Completed unit tests for GetClockRequest and GetClockResponse.

---
+++ tests/AdaSharp.Wallet.Tests/Network/GetClockResponseTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using AdaSharp.Network;
+using AdaSharp.Tests.TestData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RestSharp;
+
+namespace AdaSharp.Tests.Network
+{
+    [TestClass]
+    public class GetClockResponseTest
+    {
+        private const string Http200TestFile = @"TestData\Node\Network\Clock\Http200.json";
+
+        [TestMethod]
+        public void Constructor_NodeReturnsHttp200_HttpStatusCodeIsOk()
+        {
+            // Assemble
+            const HttpStatusCode expectedHttpStatusCode = HttpStatusCode.OK;
+
+            var responseFromNode = TestRestResponse.LoadHttp200From(Http200TestFile);
+
+            // Act
+            var result = ConstructGetClockResponse(responseFromNode);
+
+            // Assert
+            Assert.AreEqual(expectedHttpStatusCode, result.HttpStatusCode);
+        }
+
+        [TestMethod]
+        public void Constructor_NodeReturnsHttp200_ClockStatusIsPopulated()
+        {
+            // Assemble
+            const ClockStatus expectedClockStatus = ClockStatus.Available;
+
+            var responseFromNode = TestRestResponse.LoadHttp200From(Http200TestFile);
+
+            // Act
+            var result = ConstructGetClockResponse(responseFromNode);
+
+            // Assert
+            Assert.AreEqual(expectedClockStatus, result.Status);
+        }
+
+        [TestMethod]
+        public void Constructor_NodeReturnsHttp200_OffsetIsPopulated()
+        {
+            // Assemble
+            var expectedUnitInResponse = "microsecond";
+            var expectedQuantityInResponse = -4335;
+            var responseFromNode = TestRestResponse.LoadHttp200From(Http200TestFile);
+
+            // Act
+            var response = ConstructGetClockResponse(responseFromNode);
+
+            // Assert
+            var result = response.Offset;
+
+            Assert.AreEqual(expectedQuantityInResponse, result.Quantity);
+            Assert.AreEqual(expectedUnitInResponse, result.Unit);
+        }
+
+        private GetClockResponse ConstructGetClockResponse(Mock<IRestResponse> mockedResponse)
+        {
+            return new GetClockResponse(mockedResponse?.Object);
+        }
+    }
+}
---
+++ tests/AdaSharp.Wallet.Tests/TestData/TestRestResponse.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Net;
+using Moq;
+using RestSharp;
+
+namespace AdaSharp.Tests.TestData
+{
+    public class TestRestResponse
+    {
+        public static Mock<IRestResponse> LoadHttp200From(string bodyContentFilePath)
+        {
+            var bodyContent = File.ReadAllText(bodyContentFilePath);
+
+            return MockRestResponse(HttpStatusCode.OK, bodyContent);
+        }
+
+        private static Mock<IRestResponse> MockRestResponse(HttpStatusCode statusCode, string content)
+        {
+            var mockedResponse = new Mock<IRestResponse>();
+
+            mockedResponse
+                .Setup(m => m.StatusCode)
+                .Returns(statusCode);
+
+            mockedResponse
+                .Setup(m => m.Content)
+                .Returns(content);
+
+            return mockedResponse;
+        }
+    }
+}
